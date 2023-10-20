6f8c6eb07b43562b87817dca951305174e46c1e0
Alexander Borsuk <me@alex.bio>
2023-10-20 11:32:06 +0200

    tmp

--- drape/glyph_manager.cpp
+++ drape/glyph_manager.cpp
@@ -144,15 +144,9 @@ public:
 
   ~Font()
   {
-    if (m_fontFace)
-    {
-      FREETYPE_CHECK(FT_Done_Face(m_fontFace));
-      m_fontFace = nullptr;
-    }
-    else
-    {
-      ASSERT(false, ("WTF"));
-    }
+    ASSERT(m_fontFace, ());
+    FREETYPE_CHECK(FT_Done_Face(m_fontFace));
+    m_fontFace = nullptr;
   }
 
   bool IsValid() const
